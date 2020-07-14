42ab34048802ed1568d3ebd10a97471d263111bf
Lostark13 <tushar9915@gmail.com>
2020-07-14 11:03:25 +0530

    drivers:input: Update for 4.9

--- drivers/input/touchscreen/focaltech_touch/focaltech_test/src/focaltech_test.c
+++ drivers/input/touchscreen/focaltech_touch/focaltech_test/src/focaltech_test.c
@@ -156,7 +156,7 @@ static int fts_test_get_ini_size(char *config_name)
 		return -EIO;
 	}
 
-	inode = pfile->f_dentry->d_inode;
+	inode = pfile->f_path.dentry->d_inode;
 
 	fsize = inode->i_size;
 	filp_close(pfile, NULL);
@@ -191,7 +191,7 @@ static int fts_test_read_ini_data(char *config_name, char *config_buf)
 		return -EIO;
 	}
 
-	inode = pfile->f_dentry->d_inode;
+	inode = pfile->f_path.dentry->d_inode;
 
 	fsize = inode->i_size;
 	old_fs = get_fs();
