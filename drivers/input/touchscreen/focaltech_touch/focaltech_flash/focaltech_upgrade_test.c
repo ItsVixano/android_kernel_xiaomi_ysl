79bf93d9a58ccb409c7bd4b6ce016994544eec1e
Lostark13 <tushar9915@gmail.com>
2020-07-28 21:27:15 +0530

    drivers: Convert To Wakeup Sources

--- drivers/input/touchscreen/focaltech_touch/focaltech_flash/focaltech_upgrade_test.c
+++ drivers/input/touchscreen/focaltech_touch/focaltech_flash/focaltech_upgrade_test.c
@@ -35,7 +35,8 @@
 *****************************************************************************/
 #include "../focaltech_core.h"
 #include "../focaltech_flash.h"
-#include <linux/wakelock.h>
+#include <linux/device.h>
+#include <linux/pm_wakeup.h>
 #include <linux/timer.h>
 
 /*****************************************************************************
@@ -46,7 +47,7 @@
 /*****************************************************************************
 * Global variable or extern global variabls/functions
 *****************************************************************************/
-struct wake_lock ps_lock;
+struct wakeup_source ps_lock;
 
 #define FTS_DEBUG_UPGRADE(fmt, args...) do{\
 					                            printk(KERN_ERR "[FTS][UPGRADE]:##############################################################################\n");\
@@ -141,9 +142,9 @@ int fts_ctpm_auto_upgrade(struct i2c_client *client)
 	static int uc_ErrorTimes = 0;
 	static int uc_UpgradeTimes = 0;
 
-	wake_lock_init(&ps_lock, WAKE_LOCK_SUSPEND, "tp_wakelock");
+	wakeup_source_init(&ps_lock, "tp_wakelock");
 
-	wake_lock(&ps_lock);
+	__pm_stay_awake(&ps_lock);
 
 	/* (FTS_GET_VENDOR_ID_NUM == 0) */
 	g_fw_file = CTPM_FW;
@@ -172,7 +173,7 @@ int fts_ctpm_auto_upgrade(struct i2c_client *client)
 	}
 	while (uc_UpgradeTimes < (FTS_UPGRADE_TEST_NUMBER));
 
-	wake_unlock(&ps_lock);
+	__pm_relax(&ps_lock);
 
 	return 0;
 }
--- drivers/video/fbdev/msm/mdss_fb.c
+++ drivers/video/fbdev/msm/mdss_fb.c
@@ -47,7 +47,8 @@
 #include <linux/kthread.h>
 #include <linux/dma-buf.h>
 #include <linux/mdss_io_util.h>
-#include <linux/wakelock.h>
+#include <linux/device.h>
+#include <linux/pm_wakeup.h>
 #include "mdss_dsi.h"
 #include "mdss_fb.h"
 #include "mdss_mdp_splash_logo.h"
@@ -133,7 +134,7 @@ static void mdss_fb_set_mdp_sync_pt_threshold(struct msm_fb_data_type *mfd,
 static struct fb_info *prim_fbi;
 static struct delayed_work prim_panel_work;
 static atomic_t prim_panel_is_on;
-static struct wake_lock prim_panel_wakelock;
+static struct wakeup_source prim_panel_wakelock;
 static void prim_panel_off_delayed_work(struct work_struct *work)
 {
 #ifdef CONFIG_FRAMEBUFFER_CONSOLE
@@ -149,7 +150,7 @@ static void prim_panel_off_delayed_work(struct work_struct *work)
 	if (atomic_read(&prim_panel_is_on)) {
 		fb_blank(prim_fbi, FB_BLANK_POWERDOWN);
 		atomic_set(&prim_panel_is_on, false);
-		wake_unlock(&prim_panel_wakelock);
+		__pm_relax(&prim_panel_wakelock);
 	}
 
 	unlock_fb_info(prim_fbi);
@@ -1884,7 +1885,7 @@ static int mdss_fb_remove(struct platform_device *pdev)
 if (mfd->panel_info && mfd->panel_info->is_prim_panel) {
 		atomic_set(&prim_panel_is_on, false);
 		cancel_delayed_work_sync(&prim_panel_work);
-		wake_lock_destroy(&prim_panel_wakelock);
+		wakeup_source_trash(&prim_panel_wakelock);
 	}
 
 	mdss_fb_remove_sysfs(mfd);
@@ -2620,7 +2621,7 @@ static int mdss_fb_blank(int blank_mode, struct fb_info *info)
 	if ((info == prim_fbi) && (blank_mode == FB_BLANK_UNBLANK) &&
 		atomic_read(&prim_panel_is_on)) {
 		atomic_set(&prim_panel_is_on, false);
-		wake_unlock(&prim_panel_wakelock);
+		__pm_relax(&prim_panel_wakelock);
 		cancel_delayed_work_sync(&prim_panel_work);
 		return 0;
 	}
@@ -3315,7 +3316,7 @@ static int mdss_fb_register(struct msm_fb_data_type *mfd)
 		prim_fbi = fbi;
 		atomic_set(&prim_panel_is_on, false);
 		INIT_DELAYED_WORK(&prim_panel_work, prim_panel_off_delayed_work);
-		wake_lock_init(&prim_panel_wakelock, WAKE_LOCK_SUSPEND, "prim_panel_wakelock");
+		wakeup_source_init(&prim_panel_wakelock, "prim_panel_wakelock");
 	}	
 
 	return 0;
@@ -5750,16 +5751,16 @@ int mdss_prim_panel_fb_unblank(int timeout)
 			return 0;
 		}
 
-		wake_lock(&prim_panel_wakelock);
+		__pm_stay_awake(&prim_panel_wakelock);
 		ret = fb_blank(prim_fbi, FB_BLANK_UNBLANK);
 		if (!ret) {
 			atomic_set(&prim_panel_is_on, true);
 			if (timeout > 0)
 				schedule_delayed_work(&prim_panel_work, msecs_to_jiffies(timeout));
 			else
-				wake_unlock(&prim_panel_wakelock);
+				__pm_relax(&prim_panel_wakelock);
 		} else
-			wake_unlock(&prim_panel_wakelock);
+			__pm_relax(&prim_panel_wakelock);
 		unlock_fb_info(prim_fbi);
 #ifdef CONFIG_FRAMEBUFFER_CONSOLE
 		console_unlock();
